a3bbd8cddb734e8a82cdb2133a05b1fcdf435da7
Stian Sigvartsen <stian_sigvartsen@hotmail.com>
2015-01-26 22:52:05 +0000

    Missing licence info

--- docroot/WEB-INF/src/com/wordpress/metaphorm/authProxy/oauthClient/OAuthExpectationFailedException.java
+++ docroot/WEB-INF/src/com/wordpress/metaphorm/authProxy/oauthClient/OAuthExpectationFailedException.java
@@ -1,5 +1,24 @@
 package com.wordpress.metaphorm.authProxy.oauthClient;
 
+/**
+ * Copyright (c) 2014-present Stian Sigvartsen. All rights reserved.
+ *
+ * This file is part of Social Apps Proxy.
+ *
+ * Social Apps Proxy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Social Apps Proxy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Social Apps Proxy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class OAuthExpectationFailedException extends Exception {
 
 	private static final long serialVersionUID = 1L;
